1e5415e489f16e2443ebb2c11003b9e01d2198d2
sonorousduck <sonorousduck@gmail.com>
2021-12-03 23:16:50 -0700

    Initial Commit

---
+++ createSong.py
@@ -0,0 +1,139 @@
+import pickle
+import numpy as np
+from keras.utils import np_utils
+from music21 import instrument, note, chord, stream
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, BatchNormalization
+
+
+
+def prepare_sequences(notes, n_vocab):
+
+    sequence_length = 100
+
+    pitchnames = sorted(set(item for item in notes))
+
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+    network_input = []
+    network_output = []
+
+    for i in (0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # Makes the input into a form that LSTMs can understand
+
+    network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
+
+    # Normalize
+    network_input = network_input / float(n_vocab)
+
+    network_output = np_utils.to_categorical(network_output)
+
+    return network_input, network_output
+
+def create_network(network_input, n_vocab):
+    model = Sequential()
+    model.add(LSTM(512, input_shape=(network_input.shape[1], network_input.shape[2]), return_sequences=True))
+    model.add(LSTM(512))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(512, activation="relu"))
+    model.add(Dense(n_vocab, activation="softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.summary()
+
+    model.load_weights('weights.hdf5')
+
+    return model
+
+def create_midi(prediction_output):
+    offset = 0
+    output_notes = []
+
+    for pattern in prediction_output:
+        # If the pattern is a chord
+        if ('.' in pattern) or pattern.isdigit():
+            notes_in_chord = pattern.split('.')
+            notes = []
+            for current_note in notes_in_chord:
+                new_note = note.Note(int(current_note))
+                new_note.storedInstrument = instrument.Piano()  # Generates piano midi
+                notes.append(new_note)
+            new_chord = chord.Chord(notes)
+            new_chord.offset = offset
+            output_notes.append(new_chord)
+        # If the pattern is a note instead
+        else:
+            new_note = note.Note(pattern)
+            new_note.offset = offset
+            new_note.storedInstrument = instrument.Piano()  # Generates piano music
+            output_notes.append(new_note)
+
+        offset += 0.5
+
+        midi_stream = stream.Stream(output_notes)
+
+        midi_stream.write('midi', fp="test_output.midi")
+
+def generate_notes(model, network_input, pitchnames, n_vocab):
+    """ Generate notes from the neural network based on a sequence of notes"""
+
+    # Start somewhere random in the sequence
+    start = np.random.randint(0, len(network_input) - 1)
+
+    int_to_note = dict((number, note) for number, note in enumerate(pitchnames))
+
+    pattern = network_input[start]
+    prediction_output = []
+
+
+    # Generate 500 notes
+    for note_index in range(500):
+        prediction_input = np.reshape(pattern, (1, len(pattern), 1))
+        prediction_input = prediction_input / float(n_vocab)  # Normalizes the input
+
+        prediction = model.prediction(prediction_input, verbose=0)
+
+        index = np.argmax(prediction)
+
+        result = int_to_note[index]
+        prediction_output.append(result)
+
+        pattern.append(result)
+
+        pattern.append(index)
+        pattern = pattern[1:len(pattern)]
+
+    return prediction_output
+
+
+
+def generate():
+    """ Generate a midi file """
+
+    with open("data/notes", "rb") as filepath:
+        notes = pickle.load(filepath)
+
+    # Get all pitch names
+    pitchnames = sorted(set(item for item in notes))
+
+    # Get all pitch names
+    n_vocab = len(set(notes))
+
+    network_input, normalized_input = prepare_sequences(notes, pitchnames, n_vocab)
+    model = create_network(normalized_input, n_vocab)
+    prediction_output = generate_notes(model, network_input, pitchnames, n_vocab)
+    create_midi(prediction_output)
+
+
+if __name__ == "__main__":
+    generate()
---
+++ musicGenerationAgent.py
@@ -0,0 +1,114 @@
+import glob
+import pickle
+import numpy as np
+from music21 import converter, instrument, note, chord
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, BatchNormalization
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+
+
+def prepare_sequences(notes, n_vocab):
+
+    sequence_length = 100
+
+    pitchnames = sorted(set(item for item in notes))
+
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+    network_input = []
+    network_output = []
+
+    for i in range(0, len(notes) - sequence_length):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # Makes the input into a form that LSTMs can understand
+
+    network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
+
+    # Normalize
+    network_input = network_input / float(n_vocab)
+
+    network_output = np_utils.to_categorical(network_output)
+
+    return network_input, network_output
+
+
+def train_network():
+    notes = get_notes()
+
+    # This gives the amount of pitch names
+    n_vocab = len(set(notes))
+
+    network_input, network_output = prepare_sequences(notes, n_vocab)
+
+    model = create_network(network_input, n_vocab)
+
+    train(model, network_input, network_output)
+
+
+def get_notes():
+    notes = []
+
+    for file in glob.glob("music/*.midi"):
+        midi = converter.parse(file)
+
+
+        try: # This is if the file has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            notes_to_parse = s2.parts[0].recurse()
+        except:
+            notes_to_parse = midi.flat.notes
+
+
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notes.append('.'.join(str(n) for n in element.normalOrder))
+
+        with open('data/notes', 'wb') as filepath:
+            pickle.dump(notes, filepath)
+
+        return notes
+
+
+
+def create_network(network_input, n_vocab):
+    model = Sequential()
+    model.add(LSTM(512, input_shape=(network_input.shape[1], network_input.shape[2]), return_sequences=True))
+    model.add(LSTM(512))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(512, activation="relu"))
+    model.add(Dense(n_vocab, activation="softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.summary()
+    return model
+
+def train(model, network_input, network_output):
+    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=1,
+        save_best_only=True,
+        mode='min'
+    )
+
+    callbacks_list = [checkpoint]
+
+    model.fit(network_input, network_output, epochs=1000, batch_size=128, callbacks=callbacks_list)
+
+
+if __name__ == "__main__":
+    # get_notes()
+    train_network()
